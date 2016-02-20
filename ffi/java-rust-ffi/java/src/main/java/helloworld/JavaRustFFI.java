4f391fced3305394a591c246410711bb25599a17
redtankd <redtankd@qq.com>
2016-02-21 01:17:15 +0800

    add java-rust-ffi example

---
+++ ffi/java-rust-ffi/java/src/main/java/helloworld/JavaRustFFI.java
@@ -0,0 +1,29 @@
+package helloworld;
+
+import java.io.File;
+
+import jnr.ffi.LibraryLoader;
+
+import static java.lang.System.mapLibraryName;
+
+public class JavaRustFFI {
+
+    public static interface RustLib {
+        int double_input(int i);
+    }
+
+    public static String getLibraryPath(String dylib) {
+        File f = new File(JavaRustFFI.class.getClassLoader().getResource(mapLibraryName(dylib)).getFile());
+        return f.getParent();
+    }
+
+    public static void main(String[] args) {
+        String dylib = "double_input";
+        System.setProperty("jnr.ffi.library.path", getLibraryPath(dylib));
+
+        RustLib rlib = LibraryLoader.create(RustLib.class).load(dylib);
+        int r = rlib.double_input(20);
+
+        System.out.println("Result from rust double_input:  " + r);
+    }
+}
