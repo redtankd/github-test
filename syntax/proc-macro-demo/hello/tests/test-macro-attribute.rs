5a2c321321920bd8c50b94d796317485e5876c38
redtankd <redtankd@qq.com>
2019-02-02 16:09:01 +0800

    proc macro demo for modifying a struct

--- syntax/proc-macro-demo/hello/tests/test-macro-attribute.rs
+++ syntax/proc-macro-demo/hello/tests/test-macro-attribute.rs
@@ -1,7 +1,19 @@
 #[hello_macro::hello]
 fn wrapped_function() {}
 
+#[hello_macro::struct_extension]
+struct DummyStruct {
+    my_field: i32,
+}
+
 #[test]
 fn works() {
-   assert_eq!(42, wrapped_function());
+    assert_eq!(42, wrapped_function());
+
+    let dummy_struct = DummyStruct {
+        my_field: 32,
+        append: String::from("someone@example.com"),
+    };
+    assert_eq!(32, dummy_struct.my_field);
+    assert_eq!(String::from("someone@example.com"), dummy_struct.append);
 }
