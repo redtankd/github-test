4349cd0d3f33f5fa4557d84f85fa11c07935e031
redtankd <redtankd@qq.com>
2020-06-06 22:27:32 +0800

    branch coverage

--- code-coverage/src/lib.rs
+++ code-coverage/src/lib.rs
@@ -9,16 +9,33 @@ pub fn sub(a: i32, b: i32) -> isize {
     (a + b) as isize
 }
 
-pub fn branch(a: i32) -> isize {
+pub fn branch_not_covered(a: i32) -> isize {
     if a < 10 {
         0
-    } else {
+    } else if a == 10 {
         1
+    } else {
+        2
+    }
+}
+
+pub fn branch_covered(a: i32) -> isize {
+    if a < 10 {
+        0
+    } else if a == 10 {
+        1
+    } else {
+        2
     }
 }
 
 #[test]
 fn test() {
     assert_eq!(10isize, add(4, 6));
-    assert_eq!(0isize, branch(4));
+
+    assert_eq!(0isize, branch_not_covered(4));
+    assert_eq!(2isize, branch_not_covered(20));
+
+    assert_eq!(0isize, branch_covered(4));
+    assert_eq!(1isize, branch_covered(10));
 }
